9b48c712ad2b8068b0532c487feb3e5cb31d2ec3
IvanGorbunov <falseprogrammerfirst@gmail.com>
2024-02-27 12:55:27 +0300

    fixed tests

--- src/tests/test_last_update_dates.py
+++ src/tests/test_last_update_dates.py
@@ -2,26 +2,9 @@ from unittest.mock import patch
 
 import httpx
 import pytest
-import pytest_asyncio
-from typing import AsyncIterator
 
 from app.api.v1.currency.services.currency_helper import CurrencyHelper
-from app.main import app
 from app.config import settings
-
-
-@pytest_asyncio.fixture
-async def ac() -> AsyncIterator[httpx.AsyncClient]:
-    async with httpx.AsyncClient(app=app, base_url="http://testserver") as client:
-        yield client
-
-
-# @pytest.mark.asyncio
-# async def test_last_update_dates_empty(ac: httpx.AsyncClient):
-#     # Test the last_update_dates endpoint
-#     response = await ac.get(settings.API_V1_STR + "/currencies/last_update_dates")
-#     assert response.status_code == 200
-#     assert "data" in response.json()
 
 
 @pytest.mark.asyncio
--- src/tests/test_update_exchange_rates.py
+++ src/tests/test_update_exchange_rates.py
@@ -2,29 +2,21 @@ from unittest.mock import patch
 
 import httpx
 import pytest
-import pytest_asyncio
-from typing import AsyncIterator
 
 from app.api.v1.currency.services.currency_helper import CurrencyHelper
-from app.main import app
 from app.config import settings
 
-
-@pytest_asyncio.fixture
-async def client() -> AsyncIterator[httpx.AsyncClient]:
-    async with httpx.AsyncClient(app=app, base_url="http://testserver") as client:
-        yield client
 
 
 @pytest.mark.asyncio
 @patch.object(CurrencyHelper, "update_exchange_rates")
 async def test_update_exchange_rates_success(
     mock_update_exchange_rates,
-    client: httpx.AsyncClient,
+    ac: httpx.AsyncClient,
 ):
     mock_update_exchange_rates.return_value = "Exchange rates updated successfully"
 
-    response = await client.post(
+    response = await ac.post(
         settings.API_V1_STR + "/currencies/update_exchange_rates/", json={}
     )
     assert response.status_code == 200
@@ -35,11 +27,11 @@ async def test_update_exchange_rates_success(
 @patch.object(CurrencyHelper, "update_exchange_rates")
 async def test_update_exchange_rates_error(
     mock_update_exchange_rates,
-    client: httpx.AsyncClient,
+    ac: httpx.AsyncClient,
 ):
     mock_update_exchange_rates.return_value = None
 
-    response = await client.post(
+    response = await ac.post(
         settings.API_V1_STR + "/currencies/update_exchange_rates/", json={}
     )
     assert response.status_code == 503
